a1393b2ed89ffb274b35d6bd9a6a330f5cf3b8e8
Yevhenii <norman78795@gmail.com>
2022-10-07 16:49:41 +0300

    add auth fix

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,15 +1,7 @@
-import React, {useState} from "react";
 import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import {Button, Modal, Form} from "react-bootstrap";
 
 function NavBar() {
-
-    const [show, setShow] = useState(false);
-
-    const handelClose = () => setShow(false);
-    const handleShow = () => setShow(true);
 
     return (
         <>
