c02264c11620a865676ad7c6c4e50a9747a12145
Yevhenii <norman78795@gmail.com>
2022-10-07 16:55:38 +0300

    add auth fix 2

--- src/Components/SignUp/SignUp.jsx
+++ src/Components/SignUp/SignUp.jsx
@@ -22,7 +22,7 @@ const SignUp = () => {
                 }));
                 navigate('/');
             })
-            .catch(console.error)
+            .catch(() => alert('Invalid user'))
     }
 
     return (
