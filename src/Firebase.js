4a5c6f38661355bdf64aee300b6b52784a7abc96
Yevhenii <norman78795@gmail.com>
2022-10-07 19:48:56 +0300

    add auth fix 3

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    apiKey: "AIzaSyDkDbtuIFS65-AKcxZaTp8nEt6HeH89tFo",
+    authDomain: "spacex-4aca2.firebaseapp.com",
+    projectId: "spacex-4aca2",
+    storageBucket: "spacex-4aca2.appspot.com",
+    messagingSenderId: "325798377878",
+    appId: "1:325798377878:web:5c1b17a5b13064ce37c5b7",
 };
 
 initializeApp(firebaseConfig);
